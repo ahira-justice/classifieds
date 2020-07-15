8f2166a0e053eb2ac4e89fb2538c9df74fd21515
Ahira <justiceahira@gmail.com>
2020-07-15 01:40:56 +0100

    Create item app

--- app/settings.py
+++ app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'core',
     'user',
     'profile',
+    'item',
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/profile/', include('profile.urls')),
+    path('api/item/', include('item.urls')),
 ]
