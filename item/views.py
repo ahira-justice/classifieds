699cddd2ad55281c013162cb97f53336138f285d
Ahira <justiceahira@gmail.com>
2020-07-21 01:45:26 +0100

    Fix codequality

--- item/views.py
+++ item/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from item.serializers import ItemSerializer
